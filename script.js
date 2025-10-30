8b9b12576176a490b6143515ac4d48baa1fd7c6b
Amanda Oliv <137652750+OlivFre@users.noreply.github.com>
2025-10-30 19:09:43 -0300

    últimos ajustes no container swiper

--- script.js
+++ script.js
@@ -2,19 +2,13 @@ const swiper = new Swiper('.slide-wrapper', {
 
 loop: true,
 grabCursor: true,
-spaceBetween: true,
+spaceBetween: 30,
 
   // If we need pagination
 pagination: {
     el: '.swiper-pagination',
     clickable: true,
     dynamicBullets: true,
-},
-
-  // Navigation arrows
-navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
 },
 
 //responsive breakpoints
